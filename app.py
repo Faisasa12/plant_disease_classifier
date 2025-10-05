673e34135c849f04dadbf1cdb8949cc28b5e7d15
Faisasa12 <faisalmufadhi@gmail.com>
2025-10-05 20:05:08 +0100

    Added examples to choose

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 import torch
+import os
 
 from models.model import MyCNN
 from utils.transforms import get_transforms
@@ -25,16 +26,43 @@ st.title("Plant Disease Classifier")
 st.markdown("Upload a leaf image to detect disease and view model attention with Grad-CAM.")
 st.markdown("**If 224 x 224 image is not used, then image will be resized**")
 
+col1, col2 = st.columns(2)
+
+EXAMPLE_IMAGE_DIR = "examples"
+
+example_images = {
+    "Tomato Leaf Mold correct": "0a9b3ff4-5343-4814-ac2c-fdb3613d4e4d___Crnl_L.Mold 6559.JPG",
+    "Tomato Leaf Mold wrong": "1fa78650-4f81-4c8f-9190-91f1057d1158___Crnl_L.Mold 9083.JPG",
+    "Potato healthy wrong": "0b3e5032-8ae8-49ac-8157-a1cac3df01dd___RS_HL 1817.JPG",
+    "Potato healthy correct": "2ccb9ee9-faac-4d32-9af5-29497fa2e028___RS_HL 1837.JPG"
+}
+
+example_choice = st.selectbox("Or choose an example image", ["None"] + list(example_images.keys()))
+
+if example_choice != "None":
+    example_path = os.path.join(EXAMPLE_IMAGE_DIR, example_images[example_choice])
+    
+    example_image = Image.open(example_path).convert("RGB")
+    
+    with col1:
+        st.image(example_image, caption=f"Example: {example_choice}", use_container_width=True)
+
+    input_image = example_image
+else:
+    input_image = None
+    
+    
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file:
-    image = Image.open(uploaded_file).convert("RGB")
-    col1, col2 = st.columns(2)
+    input_image = Image.open(uploaded_file).convert("RGB")
     
     with col1:
-        st.image(image, caption="Uploaded Image", use_container_width=True)
+        st.image(input_image, caption="Uploaded Image", use_container_width=True)
 
-    input_tensor = transform(image)
+
+if input_image:
+    input_tensor = transform(input_image)
     confidence, class_idx, predicted_class = predict(model, input_tensor, idx_to_class)
     
     activation_map = cam_extractor(class_idx, model(input_tensor.unsqueeze(0)))
